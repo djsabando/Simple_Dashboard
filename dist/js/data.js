cdcab3f488ceff1f6cc94ca62623e8369eac9bfa
djsabando <douglas_sabando@hotmail.es>
2023-06-19 23:43:35 -0500

    Gráfico Barras Implementado

--- dist/js/data.js
+++ dist/js/data.js
@@ -1,20 +1,30 @@
 
 
 let load = (data) => {
+
+    // zona horaria
     let zonaHoraria = data["timezone"]
     let zonaHorariaHTML = document.getElementById("zonaHoraria")
     zonaHorariaHTML.textContent = zonaHoraria;
 
+    // temperatuta 
     let temperature = data['hourly']['temperature_2m'][0]
+    console.log(temperature);
     let temperaturaHTML = document.getElementById("temperatura")
-    temperaturaHTML.textContent = temperature;
+    temperaturaHTML.textContent = temperature + " °C";
 
-    // let uvIndice = data['daily']['uv_index_max'][0]
-    // let uvIndiceHTML = document.getElementById("uvIndex")
-    // uvIndiceHTML.textContent = uvIndice;
+    // UV Index
+    let uvIndice = data['daily']['uv_index_max'][0]
+    let uvIndiceHTML = document.getElementById("uvIndex")
+    uvIndiceHTML.textContent = uvIndice;
 
+    // elevación
+    let elevacion = data["elevation"]
+    let elevacionHTML = document.getElementById("elevacionId")
+    elevacionHTML.textContent = elevacion + " m";
 
     plot(data);
+    plot2(data);
 
 }
 
@@ -41,6 +51,52 @@ let plot = (data) => {
     const chart = new Chart(ctx, config)
 }
 
+let plot2 = (data) => {
+
+    const ctx = document.getElementById('myChart2');
+
+    const dataset = {
+        labels: data.daily.time,
+        datasets: [{
+            label: 'Índice UV Semanal',
+            data: data.daily.uv_index_max,
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(255, 159, 64, 0.2)',
+                'rgba(255, 205, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(201, 203, 207, 0.2)'
+            ],
+            borderColor: [
+                'rgb(255, 99, 132)',
+                'rgb(255, 159, 64)',
+                'rgb(255, 205, 86)',
+                'rgb(75, 192, 192)',
+                'rgb(54, 162, 235)',
+                'rgb(153, 102, 255)',
+                'rgb(201, 203, 207)'
+            ],
+            borderWidth: 1
+        }]
+    };
+
+    const config = {
+        type: 'bar',
+        data: dataset,
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        },
+    };
+
+    const chart = new Chart(ctx, config)
+}
+
 
 let loadInocar = () => {
 
@@ -61,12 +117,6 @@ let loadInocar = () => {
             let contenedorMareas = xml.getElementsByClassName('container-fluid')[0];
             let contenedorHTML = document.getElementById('table-container');
             contenedorHTML.innerHTML = contenedorMareas.innerHTML;
-
-
-
-
-
-
         })
         .catch(console.error);
 
@@ -83,25 +133,25 @@ let loadInocar = () => {
     function () {
         let meteo = localStorage.getItem('meteo');
 
-        if (meteo == null) {
-            let URL = 'https://api.open-meteo.com/v1/forecast?latitude=-2.13&longitude=-79.90&hourly=temperature_2m&daily=weathercode,sunset,uv_index_max&timezone=auto';
-            fetch(URL)
-                .then(response => response.json())
-                .then(data => {
-                    load(data);
-                    /* GUARDAR DATA EN LA MEMORIA */
-                    localStorage.setItem("meteo", JSON.stringify(data));
+        // if (meteo == null) {
+        let URL = 'https://api.open-meteo.com/v1/forecast?latitude=-2.13&longitude=-79.90&hourly=temperature_2m&daily=weathercode,sunset,uv_index_max&timezone=auto';
+        fetch(URL)
+            .then(response => response.json())
+            .then(data => {
+                load(data);
+                /* GUARDAR DATA EN LA MEMORIA */
+                localStorage.setItem("meteo", JSON.stringify(data));
 
 
-                })
-                .catch(console.error);
+            })
+            .catch(console.error);
 
-        } else {
+        // } else {
 
-            /* CARGAR DATA DESDE LA MEMORIA */
-            load(JSON.parse(meteo));
+        //     /* CARGAR DATA DESDE LA MEMORIA */
+        //     load(JSON.parse(meteo));
 
-        }
+        // }
 
         // loadInocar();
 
